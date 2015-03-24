90a07aebb6b6c4e10c3f816e986d3c5196f9f0e4
Luke Arnott-Job <larnottjob@googlemail.com>
2015-03-24 16:15:55 +0000

    Dragging mouse content is complete. Dragging DOMs is complete. Mouse.js is not constructor based.

--- js/ui/mouse.js
+++ js/ui/mouse.js
@@ -1,47 +1,53 @@
 //set mouseFunction to function to be launched on mousedown.
 //call trackMouse
-
-
-
-var mouse= {x: -1, y: -1};
-var mousedownFunction;
-var elementEffected;
-var originalPointerPosition;
-var originalDivPosition;
-var dragDiff;
-function trackMouse(funcArg, element){
-	mousedownFunction = funcArg;
-	elementEffected = element;
-	mousePosition(false);
-	originalMousePosition(element);
-	document.addEventListener("mousemove", mousePosition);
+var mouseElement;
+var originalMousePosition;
+var eventFunction;
+var elementOffset;
+function mousePosition(){
+    var mouse = {x: event.clientX, y: event.clientY};
+    return mouse;
 }
-function stopTrackMouse(){
-	document.removeEventListener("mousemove", mousePosition);
-	
-	mousedownFunction.clear;
+function elementPosition(){
+    var offset = {x: parseInt(window.getComputedStyle(mouseElement, null).getPropertyValue('left').replace('px', ''), 10), y: parseInt(window.getComputedStyle(mouseElement, null).getPropertyValue('top').replace('px', ''), 10)};
+   
+    return offset;
 }
-function mousePosition(runFunction){
-	mouse.x = event.clientX;
-	mouse.y = event.clientY;
-	if(runFunction){
-	mousedownFunction();
-	}
+function dragDOM(element){
+    mouseElement = element;
+    originalMousePosition = mousePosition();
+    elementOffset = elementPosition();
+    document.addEventListener('mousemove', draggingDOM);
+    document.addEventListener('mouseup', function(){document.removeEventListener('mousemove', draggingDOM);});
 }
-
-function originalMousePosition(element){
-	console.log();
-	var left = window.getComputedStyle(element,null).getPropertyValue("left").replace('px', '');
-	var top = window.getComputedStyle(element,null).getPropertyValue("top").replace('px', '');
-	dragDiff = {x: mouse.x- left,y: mouse.y - top};
-	
+function draggingDOM(){
+    var mouse = mousePosition();
+    mouseElement.style.left = mouse.x - originalMousePosition.x + elementOffset.x;
+    mouseElement.style.top = mouse.y - originalMousePosition.y + elementOffset.y;
+}
+function contextOffset(){
+    var offset = {x: mouseElement.offsetX, y: mouseElement.offsetY};
+    return offset;
 }
 
-function dragDOM(){
-
-	elementEffected.style.left = mouse.x - dragDiff.x;
-	elementEffected.style.top = mouse.y - dragDiff.y;
+//Stage interaction.
+function dragContext(object){
+    mouseElement = object;
+    originalMousePosition = mousePosition();
+    elementOffset = contextOffset();
+    document.addEventListener('mousemove', draggingContext);
+    document.addEventListener('mouseup', function(){document.removeEventListener('mousemove', draggingContext);});
+    
 }
-function positionDiff(){
-
+function draggingContext(){
+    var mouse = mousePosition();
+    mouseElement.offsetX = mouse.x - originalMousePosition.x + elementOffset.x;
+    mouseElement.offsetY = mouse.y - originalMousePosition.y + elementOffset.y;
+}
+function mouseCircleCollision(object){
+    var mouse = mousePosition();
+    var radius = Math.sqrt(Math.pow(object.x - mouse.x + mainStage.offsetX, 2) + Math.pow(object.y -mouse.y + mainStage.offsetY, 2));
+    if(radius < object.radius){
+        alert('collision');
+    }
 }
---
+++ js/ui/stage.js
@@ -0,0 +1,29 @@
+function Stage(x, y, width, height, element){
+    this.x = x;
+    this.y = y;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.width = width;
+    this.height = height;
+    this.element = document.getElementById(element);
+    this.context;
+    this.init = function(){//Initialise the stage with the width, height, top, left and drawing.
+        this.element.width = this.width;
+        this.element.height = this.height;
+        this.element.style.left = this.x;
+        this.element.style.top = this.y;
+        this.context = this.element.getContext("2d");
+    }
+    this.init();
+}
+Stage.prototype.drawRect = function(){//x, y, width, height, colour.
+
+    if(arguments[arguments.length -1] === true){//If the rectangle to be drawn is in an array or not.
+        this.context.fillStyle = arguments[0][4];
+        this.context.fillRect(arguments[0][0] + this.offsetX, arguments[0][1] + this.offsetY, arguments[0][2], arguments[0][3]);
+        
+    }else{
+        this.context.fillStyle = arguments[4];
+        this.context.fillRect(arguments[0] + this.offsetX, arguments[1] + this.offsetY, arguments[2], arguments[3]);
+    }
+}
